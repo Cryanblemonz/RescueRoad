416851e5f21f86d1d40da8ab58b612fac6c98792
George Bryan Clemons <gbc466@gmail.com>
2023-06-01 20:15:57 -0500

    formatted PetDisplay into grid

--- client/src/LikedPetsFiles/PetDisplay.jsx
+++ client/src/LikedPetsFiles/PetDisplay.jsx
@@ -1,21 +1,34 @@
-function PetDisplay(props){
-        return(
-                <div className="pet-display">
-                        <h1>{props.name}</h1>
-                        <img src={props.image}></img>
-                        <p>{props.breed}</p>
-                        <p>{props.age}</p>
-                        <p>{props.goodWithKids}</p>
-                        <p>{props.goodWithCats}</p>
-                        <p>{props.goodWithDogs}</p>
-                        <p>{props.description}</p>
-                        <h2>Contact</h2>
-                        <p>{props.zipCode}</p>
-                        <p>{props.contactName}</p>
-                        <p>{props.contactPhone}</p>
-                        <p>{props.contactEmail}</p>
+function PetDisplay(props) {
+    return (
+        <div className="display-wrapper">
+            <h1>{props.name}</h1>
+            <div className="pet-display">
+                <div className="pet-display-image grid-item">
+                    <img src={props.image}></img>
                 </div>
-        )
+                <div className="pet-display-info grid-item">
+                    <p>{props.breed}</p>
+                    <p>{props.age}</p>
+                    <p>{props.goodWithKids}</p>
+                    <p>{props.goodWithCats}</p>
+                    <p>{props.goodWithDogs}</p>
+                </div>
+                <div className="pet-display-description grid-item">
+                    <p>{props.description}</p>
+                </div>
+                <div className="pet-display-contact grid-item">
+                    <h2>Contact</h2>
+                    <p>{props.zipCode}</p>
+                    <hr></hr>
+                    <p>{props.contactName}</p>
+                    <hr></hr>
+                    <p>{props.contactPhone}</p>
+                    <hr></hr>
+                    <p>{props.contactEmail}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default PetDisplay;
