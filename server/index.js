78b8c85ec139c1d33e43e9820c9ba5c064227afb
George Bryan Clemons <gbc466@gmail.com>
2023-06-19 15:08:19 -0500

    removed wildcard origin

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: "*",
+        origin: "https://rescue-road-frontend.onrender.com",
         methods: ["POST", "GET"],
     })
 );
