726c785f718d8fd229dd141733547d7b69c112eb
George Bryan Clemons <gbc466@gmail.com>
2023-06-20 10:54:11 -0500

    added origin permission for custom domain

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: "https://rescue-road-frontend.onrender.com",
+        origin: ["https://rescue-road-frontend.onrender.com", "https://www.rescueroadpets.com"],
         methods: ["POST", "GET"],
     })
 );
