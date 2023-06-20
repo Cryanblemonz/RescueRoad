df4a01fd54277a2519654bb640f688071ea595fd
George Bryan Clemons <gbc466@gmail.com>
2023-06-20 14:15:47 -0500

    express

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3001', // Replace with the origin of your frontend
+    origin: 'http://localhost:3001', 
     methods: ['POST', 'GET'],
 }));
 
